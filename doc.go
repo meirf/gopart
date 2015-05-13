014e6538f15fc6710655bbb34f10d2bac7fa3295
Meir Fischer <meirfischer@gmail.com>
2015-05-13 02:37:42 -0400

    add doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+// The gopart package alleviates the issue of partitioning
+// collections with wide-ranging types by returning index
+// ranges that can be used on any indexable object.
+package gopart
